4ea9216e7dd6f73c4c3d6dcbd9e61729e911e42d
mauricio403 <mauriciomatango_@hotmail.com>
2021-07-12 09:39:47 -0500

    Hola Mundo

---
+++ src/screens/HolaMundoScreen.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export const HolaMundoScreen = () => {
+    return (
+        <View style={{
+            flex:1,
+            // backgroundColor:'red',
+            justifyContent: 'center',
+            
+          }}>
+            <Text style={{
+              fontSize:45,
+              textAlign:'center'
+            }}>Hola Mundo alv</Text>
+          </View>
+    )
+}
+
+
